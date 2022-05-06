c2de68691b76a78221e34c8a391a5de902e3b87a
Ulises <coughy_2@hotmail.com>
2022-05-06 14:14:01 -0500

    update files

--- prueba.groovy
+++ prueba.groovy
@@ -1,7 +1,7 @@
 @Library('java-classes@javatest') 
 import mx.com.uam.utils.test
 
-def validate = new Validate(this)
+def validate = new Validate()
 def archivo1 = "prueba.txt"
 def archivo2 = "prueba2.txt"
 def directorioname = "prueba"
