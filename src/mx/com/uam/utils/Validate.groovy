21d16c491c91f177f1c5058b4ba74a059b239057
Ulises <coughy_2@hotmail.com>
2022-05-06 17:44:14 -0500

    update files

--- src/mx/com/uam/utils/Validate.groovy
+++ src/mx/com/uam/utils/Validate.groovy
@@ -1,10 +1,7 @@
 package mx.com.uam.utils
 
 class Validate {
-    def archivo
-    Validate(archivo){
-        this.archivo = archivo
-    }
+
 def validacionSencilla(archivo){
     def comando = false
     if (fileExists(archivo)) {
